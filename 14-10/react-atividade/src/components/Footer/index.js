c41d41ab855f4f2050bb595c9ebda3721f95f4fc
João Victor Martins <joao.jovm@gmail.com>
2021-10-15 21:52:37 -0300

    chore: Task routes and components layout

--- 14-10/react-atividade/src/components/Footer/index.js
+++ 14-10/react-atividade/src/components/Footer/index.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-
+import React from 'react';
 import './style.css';
 
 const Footer = () => {
---
+++ 15-10/react-atividade-rotas/src/components/Main/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+import Router from '../../routes';
+
+export default function Main() {
+        return (
+            <div className="Main">
+                <Router />
+            </div>
+        );
+}
