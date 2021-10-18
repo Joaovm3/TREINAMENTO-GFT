9b412d84b5c1488aa7a7a1ed95d3c1f80f227182
João Victor Martins <joao.jovm@gmail.com>
2021-10-18 09:56:27 -0300

    fix: Create an input with ref into Contact Page

--- 15-10/react-atividade-rotas/src/pages/Contato/index.js
+++ 15-10/react-atividade-rotas/src/pages/Contato/index.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React,  { useRef } from 'react';
 import './style.css';
 
 export default function Contato() {
-        return (
-            <div className="Contato">
-                Contato!
-            </div>
-        );
+
+    const inputRef = useRef(null);
+
+    const handleFocus = () => {
+        inputRef.current?.focus();
+        inputRef.current.value = 'O input foi focalizado!';
+    }
+
+    return (
+        <div className="Contato">
+            Página de Contato
+            <p>
+                <input 
+                    ref={inputRef}
+                    type="text"
+                    placeholder="Clique no botão abaixo"
+                />
+            </p>
+            
+            <button onClick={handleFocus}> Realizar Foco </button>
+        </div>
+    );
 }
